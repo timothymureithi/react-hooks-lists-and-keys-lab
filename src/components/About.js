4e4f45c429d8720fec518004e86ec3d2c5c842d2
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-09-23 09:59:25 +0300

    completed lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Links from "./Links";
 
+
 function About({ bio, links }) {
   return (
     <div id="about">
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,20 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  //created navlink varible 
+  const navLinks = links.map((link) => (
+    <a key={link} href={"#" + link}>
+      {link}
+    </a>
+  ));
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  //display of navlinks
+  return (
+    <nav>
+    {navLinks}
+    </nav>
+  );
 }
+
 
 export default NavBar;
